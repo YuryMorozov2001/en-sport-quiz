ed44c9e160b80db9ea6d0f80201be3586b469819
Yury Morozov <morozovypa1@gmail.com>
2022-11-29 00:18:34 +0800

    added new blocs

--- lib/src/injector.dart
+++ lib/src/injector.dart
@@ -1,8 +1,10 @@
-import 'package:en_sport_quiz/src/logic/checker/checker_bloc.dart';
-import 'package:en_sport_quiz/src/logic/quiz/quiz_bloc.dart';
-import 'package:en_sport_quiz/src/logic/remote_config/remote_config_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'logic/checker_bloc/checker_bloc.dart';
+import 'logic/network_state/network_state_bloc.dart';
+import 'logic/quiz_bloc/quiz_bloc.dart';
+import 'logic/remote_config_bloc/remote_config_bloc.dart';
 
 class Injector extends StatelessWidget {
   const Injector({super.key, required this.router});
@@ -11,9 +13,15 @@ class Injector extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     RemoteConfigBloc remoteConfigBloc = RemoteConfigBloc();
+    NetworkStateBloc networkStateBloc = NetworkStateBloc();
+
     CheckerBloc checkerBloc = CheckerBloc(remoteConfigBloc: remoteConfigBloc);
     return MultiBlocProvider(
       providers: [
+        BlocProvider<NetworkStateBloc>(
+          create: (BuildContext context) =>
+              networkStateBloc..add(const CheckConnection()),
+        ),
         BlocProvider<QuizBloc>(
           create: (BuildContext context) => QuizBloc()..add(const Parse()),
         ),
