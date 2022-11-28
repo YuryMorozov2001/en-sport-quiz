4582394c312da0ffefa673abda4c3c6c6d75355c
Yury Morozov <morozovypa1@gmail.com>
2022-11-29 02:24:40 +0800

    deleted unnecessary files

--- lib/src/logic/quiz/quiz_event.dart
+++
@@ -1,6 +0,0 @@
-part of 'quiz_bloc.dart';
-
-@freezed
-class QuizEvent with _$QuizEvent {
-  const factory QuizEvent.parse() = Parse;
-}
--- lib/src/logic/quiz/quiz_state.dart
+++
@@ -1,8 +0,0 @@
-part of 'quiz_bloc.dart';
-
-@freezed
-class QuizState with _$QuizState {
-  const factory QuizState(
-      {List<Quiz>? quizList,
-      @Default(Status.empty) Status status}) = _QuizState;
-}
