a4dd0e4ddfb3bda801764994350d20f20d9a42d7
Yury Morozov <morozovypa1@gmail.com>
2022-11-29 00:22:16 +0800

    added webview screen

---
+++ lib/src/screens/webview/webview.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebViewScreen extends StatefulWidget {
+  const WebViewScreen({Key? key, required this.url}) : super(key: key);
+  final String url;
+
+  @override
+  State<WebViewScreen> createState() => _WebViewScreenState();
+}
+
+class _WebViewScreenState extends State<WebViewScreen> {
+  @override
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) WebView.platform = AndroidWebView();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      allowsInlineMediaPlayback: true,
+      javascriptMode: JavascriptMode.unrestricted,
+      initialUrl: widget.url,
+    );
+  }
+}
