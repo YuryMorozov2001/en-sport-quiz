998c16842cbddf417827eef3a01dd031e182ff77
Yury Morozov <morozovypa1@gmail.com>
2022-11-28 22:03:04 +0800

    hive and firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
-import 'package:en_sport_quiz/src/router/router.dart';
+import 'package:en_sport_quiz/src/injector.dart';
+import 'package:en_sport_quiz/src/service/remote_config_service.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+import 'src/router/router.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+  await FireRemoteConfigService.init();
+  await Hive.initFlutter();
+  await Hive.openBox('urlBox');
+  runApp(const Injector(
+    router: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
