2d471a42cd772bf7e816b5fb5f902efcbf3fa2b0
Yury Morozov <morozovypa1@gmail.com>
2022-11-29 02:22:00 +0800

    checking for emulator is disabled

--- lib/src/logic/checker_bloc/checker_bloc.dart
+++ lib/src/logic/checker_bloc/checker_bloc.dart
@@ -1,13 +1,9 @@
-import 'dart:async';
-
-import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:en_sport_quiz/src/const/enums.dart';
 import 'package:en_sport_quiz/src/service/emu_checker_service.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
-import '../../service/network_state_service.dart';
 import '../remote_config_bloc/remote_config_bloc.dart';
 
 part 'checker_event.dart';
@@ -24,7 +20,7 @@ class CheckerBloc extends Bloc<CheckerEvent, CheckerState> {
     on<Check>((event, emit) async {
       emit(state.copyWith(status: Status.submissionInProgress));
       // TODO : изменить на эмулятор false
-      if (!(await EmuCheckerService.checkIsEmu())) {
+      if ((await EmuCheckerService.checkIsEmu())) {
         if (urlBox.get('url') == null || urlBox.get('url') == '') {
           remoteConfigBloc.add(const FetchRemoteUrl());
         } else {
